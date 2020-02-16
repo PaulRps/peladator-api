409cf12de018d6a8b8c14301af977603e30b68fe
Paulrps <ricardopaulo18@hotmail.com>
2020-02-16 14:57:51 -0300

    removed unused classes

--- src/main/java/com/paulrps/peladator/services/impl/PlayerPositionServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package com.paulrps.peladator.services.impl;
-
-import com.paulrps.peladator.services.PlayerPositionService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class PlayerPositionServiceImpl implements PlayerPositionService {}
