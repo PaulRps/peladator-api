5c38f80233440e9dd99e9450e849d5ad84a331a0
Paulrps <ricardopaulo18@hotmail.com>
2020-03-01 15:09:24 -0300

    fixed security problem on integration-test TeamControllerIT

--- src/test/java/com/paulrps/peladator/controllers/TeamControllerIT.java
+++ src/test/java/com/paulrps/peladator/controllers/TeamControllerIT.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 // @RunWith(SpringRunner.class)
@@ -23,6 +24,7 @@ class TeamControllerIT {
   @Autowired private MockMvc mvc;
   @MockBean private TeamService service;
 
+  @WithMockUser(value = "Paulo")
   @Test
   void loadPage() throws Exception {
     SortTeamDto dto = new SortTeamDto();
